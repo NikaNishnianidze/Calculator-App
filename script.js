171ae7bb46c583d3b572e19387feb74eec959a95
NikaNishnianidze <nikanishno8@gmail.com>
2025-02-20 13:43:48 +0400

    finish styiling theme-2

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
 const display = document.getElementById("display-text");
+const slider = document.getElementById("slider-value");
+const body = document.querySelector("body");
+
+slider.addEventListener("change", () => {
+  let value = slider.value;
+  const elementsToModify = [
+    { selector: "h1", className: "h1-2" },
+    { selector: "h2", className: "h2-2" },
+    { selector: ".slider", className: "slider-2" },
+    { selector: ".p-section", className: "p-section2" },
+    { selector: ".display", className: "display2" },
+    { selector: ".keys-container", className: "keys-container2" },
+    { selector: ".delete", className: "delete2" },
+    { selector: ".reset", className: "reset2" },
+    { selector: ".calculate", className: "calculate2" },
+  ];
+
+  if (value === "2") {
+    body.classList.add("background-2");
+    elementsToModify.forEach((element) => {
+      body.querySelector(element.selector).classList.add(element.className);
+    });
+  } else {
+    body.classList.remove("background-2");
+    elementsToModify.forEach((element) => {
+      body.querySelector(element.selector).classList.remove(element.className);
+    });
+  }
+});
 
 function appendToDisplay(input) {
   display.value += input;
